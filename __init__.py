02029069d7113f744939772fa09facf04d55f158
Isaiah Harris <69725417+imharris2702@users.noreply.github.com>
2023-12-05 16:25:09 -0500

    Write data unpacker function

---
+++ __init__.py
@@ -0,0 +1,1 @@
+# Treat root as package
---
+++ data_unpacker.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd
+
+def unpack_data(path: str):
+    # Read data
+    data = pd.read_csv(path)
+    
+    # Split catagorical parameters into booleam params
+    data = pd.get_dummies(data)
+    # Fill any remaining NaN with 0
+    data = data.fillna(0)
+
+    return data
