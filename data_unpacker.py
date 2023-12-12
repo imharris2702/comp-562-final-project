5e5f4a81d39b813a9c82546968f5db51d6e0927d
Isaiah Harris <69725417+imharris2702@users.noreply.github.com>
2023-12-12 13:24:17 -0500

    Add option to normalize data

--- data_unpacker.py
+++ data_unpacker.py
@@ -1,6 +1,8 @@
 import pandas as pd
+from sklearn.preprocessing import normalize
 
-def unpack_data(path: str):
+def unpack_data(path: str, preprocess: bool = False):
+    '''Take data from house prices csv. Setting preprocess to true normalizes numerical data'''
     # Read data
     data = pd.read_csv(path)
     
@@ -8,5 +10,36 @@ def unpack_data(path: str):
     data = pd.get_dummies(data)
     # Fill any remaining NaN with 0
     data = data.fillna(0)
+
+    if preprocess:
+        # Normalize numerical data, not including numerical categories or years
+        categories = ["LotFrontage", 
+                    "LotArea", 
+                    "MasVnrArea", 
+                    "BsmtFinSF1", 
+                    "BsmtFinSF2",
+                    "BsmtUnfSF",
+                    "TotalBsmtSF",
+                    "1stFlrSF",
+                    "2ndFlrSF",
+                    "LowQualFinSF",
+                    "GrLivArea",
+                    "BsmtFullBath",
+                    "BsmtHalfBath",
+                    "FullBath",
+                    "HalfBath",
+                    "TotRmsAbvGrd",
+                    "Fireplaces",
+                    "GarageCars",
+                    "GarageArea",
+                    "WoodDeckSF",
+                    "OpenPorchSF",
+                    "EnclosedPorch",
+                    "3SsnPorch",
+                    "ScreenPorch",
+                    "PoolArea",
+                    "MiscVal",
+                    "SalePrice"]
+        data[categories] = normalize(data[categories])
 
     return data
